e78d5d805ede26613db3f0350ec8b87fc5f53a45
okzmo <mathieurossi77@gmail.com>
2023-12-31 22:06:32 +0100

    feat: add atom and code chunk

--- iris.go
+++ iris.go
@@ -1,26 +1,79 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"os"
 )
 
+type Uint32 uint32
+type Uint8 uint8
+type ToBigEndianBytes interface {
+	ToBeBytes() []byte
+}
+
+func toBeBytes(n interface{}) []byte {
+	switch t := n.(type) {
+	case uint32:
+		bytes := make([]byte, 4)
+		binary.BigEndian.PutUint32(bytes, t)
+		return bytes
+	case uint8:
+		return []byte{t}
+	default:
+		return nil
+	}
+}
+
+func paddingChunk(chunk []byte) []byte {
+	pad := len(chunk) % 4
+	fmt.Println(chunk, pad)
+	if pad != 0 {
+		chunk = append(chunk, bytes.Repeat([]byte{0}, 4-pad)...)
+	}
+
+	return chunk
+}
+
 func codeChunk() []byte {
+	var sub_size uint32 = 16
+	var instruction_set uint32 = 0
+	var opcode_max uint32 = 169
+	var label_count uint32 = 0
+	var function_count uint32 = 0
 
-	sub_size := 16
-	instruction_set := 0
-	opcode_max := 169
-	label_count := 0
-	function_count := 0
-	chunk := make([]byte, 0)
-	chunk = append(chunk, byte(sub_size))
-	chunk = append(chunk, byte(instruction_set))
-	chunk = append(chunk, byte(opcode_max))
-	chunk = append(chunk, byte(label_count))
-	chunk = append(chunk, byte(function_count))
+	var chunk []byte
+	chunk = append(chunk, toBeBytes(sub_size)...)
+	chunk = append(chunk, toBeBytes(instruction_set)...)
+	chunk = append(chunk, toBeBytes(opcode_max)...)
+	chunk = append(chunk, toBeBytes(label_count)...)
+	chunk = append(chunk, toBeBytes(function_count)...)
 
-	return []byte{}
+	var fullChunk []byte
+	fullChunk = append(fullChunk, []byte("Code")...)
+	fullChunk = append(fullChunk, toBeBytes(uint32(len(chunk)))...)
+	chunk = paddingChunk(chunk)
+	fullChunk = append(fullChunk, chunk...)
+
+	return fullChunk
+}
+
+func atomChunk(atoms []string) []byte {
+	var chunk []byte
+	chunk = append(chunk, toBeBytes(uint32(len(atoms)))...)
+	for _, atom := range atoms {
+		chunk = append(chunk, toBeBytes(uint8(len(atom)))...)
+		chunk = append(chunk, []byte(atom)...)
+	}
+
+	var fullChunk []byte
+	fullChunk = append(fullChunk, []byte("AtU8")...)
+	fullChunk = append(fullChunk, toBeBytes(uint32(len(chunk)))...)
+	chunk = paddingChunk(chunk)
+	fullChunk = append(fullChunk, chunk...)
+
+	return fullChunk
 }
 
 func main() {
@@ -32,16 +85,17 @@ func main() {
 	defer f.Close()
 
 	code := codeChunk()
+	atoms := atomChunk([]string{"iris", "hello", "world"})
 
 	beam := make([]byte, 0)
 	beam = append(beam, []byte("BEAM")...)
 	beam = append(beam, code...)
+	beam = append(beam, atoms...)
 
-	bytes := []byte{}
+	var bytes []byte
 	bytes = append(bytes, []byte("FOR1")...)
-	temp := make([]byte, 4)
-	binary.BigEndian.PutUint32(temp, uint32(len(beam)))
-	bytes = append(bytes, temp...)
+	bytes = append(bytes, toBeBytes(uint32(len(beam)))...)
+	bytes = append(bytes, beam...)
 
 	f.Write(bytes)
 }
